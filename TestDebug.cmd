5d9ddccf94f0530093244ebf58ac659bb1fb7273
Jenna Sloan <jenna3715@gmail.com>
2023-01-18 14:14:32 -0600

    Update TestDebug.cmd
    added comments

--- TestDebug.cmd
+++ TestDebug.cmd
@@ -1,8 +1,10 @@
 
 @REM reg query HKCU\SOFTWARE\Valve\Steam /v SteamPath
 
-set fezpath
+@set fezpath=
 
+@REM Extrapolate the location of the FEZ game folder
+@REM Note: does not check all Steam library locations; see steamapps/libraryfolders.vdf
 @FOR /F "tokens=2* skip=2" %%a in ('reg query "HKCU\SOFTWARE\Valve\Steam" /v "SteamPath"') do set fezpath="%%b/steamapps/common/FEZ/"
 
 cd /d %fezpath%
